38e398551eb358fbcf1ad719ac9834f493152dd8
Richards-R <130212450+Richards-R@users.noreply.github.com>
2023-07-29 01:02:26 +0900

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -3,11 +3,10 @@
 PSQL="psql --username=freecodecamp --dbname=random_number_guess -t --no-align -c"
 HIGH=1000
 RAND_INT=$((1 + $RANDOM % $HIGH))
-#echo $RAND_INT
+echo $RAND_INT
 
 #prompt the user for a username with Enter your username
 #take a username as input. allow usernames that are 22 characters
-echo -e "\n◎～◎～◎～◎～◎   Guess the number between 1-1000!  ◎～◎～◎～◎～◎\n"
 
 echo Enter your username:
 read USERNAME
@@ -18,18 +17,27 @@ then
   NEW_USER=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME')")
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 else
-  echo -e "\nWelcome back, $USERNAME! You have played xx games, and your best game took yy guesses."
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-NUMBER_GUESS() {
 echo -e "\nGuess the secret number between 1 and 1000:"
 read USERS_GUESS
+NUMBER_GUESS() {
 
 if ! [[ "$USERS_GUESS" =~ ^[0-9]+$ ]]; 
-   then echo "is a no way number"
-#then
-#echo -e "\nnot a number"
-#NUMBER_GUESS
+  then echo "That is not an integer, guess again:"
+  read USERS_GUESS
+  NUMBER_GUESS
+elif [[ "$USERS_GUESS" -gt "$RAND_INT" ]];
+  then echo "It's lower than that, guess again:"
+  read USERS_GUESS
+  NUMBER_GUESS
+elif [[ "$USERS_GUESS" -lt "$RAND_INT" ]];
+  then echo "It's higher than that, guess again:"
+  read USERS_GUESS
+  NUMBER_GUESS
+elif  [[ "$USERS_GUESS" -eq "$RAND_INT" ]];
+  then echo "You guessed it in <number_of_guesses> tries. The secret number was $RAND_INT. Nice job!"
 fi
 }
 
