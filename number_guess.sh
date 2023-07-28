a717d4bcff1f1bc3dc2b3038c72c8162195cacd7
Richards-R <130212450+Richards-R@users.noreply.github.com>
2023-07-29 03:18:12 +0900

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,32 +1,26 @@
-#! /bin/bash
+#!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=random_number_guess -t --no-align -c"
-HIGH=1000
-RAND_INT=$((1 + $RANDOM % $HIGH))
-#echo $RAND_INT
-
-#prompt the user for a username with Enter your username
-#take a username as input. allow usernames that are 22 characters
+RAND_INT=$((1 + $RANDOM % 1000))
 
 echo Enter your username:
 read USERNAME
 
 USERID=$($PSQL "SELECT user_id FROM users WHERE '$USERNAME' = username")
-GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE '$USERNAME' = username")
-BEST_GAME=$($PSQL "SELECT guess_record FROM users WHERE '$USERNAME' = username")
 GUESS_COUNT=1
 if [[ -z $USERID ]]
 then
   NEW_USER=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME')")
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 else
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE '$USERNAME' = username")
+  BEST_GAME=$($PSQL "SELECT guess_record FROM users WHERE '$USERNAME' = username")
   echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 echo -e "\nGuess the secret number between 1 and 1000:"
 read USERS_GUESS
 NUMBER_GUESS() {
-echo "GC: $GUESS_COUNT"
 if ! [[ "$USERS_GUESS" =~ ^[0-9]+$ ]]; 
   then echo "That is not an integer, guess again:"
    ((GUESS_COUNT=GUESS_COUNT+1))
@@ -45,16 +39,15 @@ elif [[ "$USERS_GUESS" -lt "$RAND_INT" ]];
 elif  [[ "$USERS_GUESS" -eq "$RAND_INT" ]];
   then 
   echo "You guessed it in $GUESS_COUNT tries. The secret number was $RAND_INT. Nice job!"
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE '$USERNAME' = username")
   ((GAMES_PLAYED=GAMES_PLAYED+1))
+  UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played ='$GAMES_PLAYED' WHERE '$USERNAME' = username")
+  GUESS_RECORD=$($PSQL "SELECT guess_record FROM users WHERE '$USERNAME' = username")
+  if [[ "$GUESS_COUNT" -lt "$GUESS_RECORD" ]];
+    then 
+    UPDATE_GUESS_RECORD=$($PSQL "UPDATE users SET guess_record = '$GUESS_COUNT' WHERE '$USERNAME' = username")
+  fi
 fi
-UPDATE_GAMES_PLAYED=$($PSQL "UPDATE users SET games_played ='$GAMES_PLAYED' WHERE '$USERNAME' = username")
-GUESS_RECORD=$($PSQL "SELECT guess_record FROM users WHERE '$USERNAME' = username")
-if [[ "$GUESS_COUNT" -lt "$GUESS_RECORD" ]];
-  then 
-  UPDATE_GUESS_RECORD=$($PSQL "UPDATE users SET guess_record = '$GUESS_COUNT' WHERE '$USERNAME' = username")
-fi
-
 }
-
 NUMBER_GUESS
 
